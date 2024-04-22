43a476308be569be62cd0cae88c1799f35ada2f3
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 19:09:40 -0500

    primer GUIA15.2

---
+++ src/main/java/com/mycompany/guia15_2/GUIA15_2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.guia15_2;
+
+import igui.pantalla15_2;
+
+/**
+ *
+ * @author nadir
+ */
+public class GUIA15_2 {
+
+    public static void main(String[] args) {
+          new pantalla15_2().setVisible(true);
+    }
+}
